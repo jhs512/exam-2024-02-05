488d163211c4325ec63eea49985edea3d2ecf575
jhs512 <jangka512@gmail.com>
2024-02-05 20:01:27 +0900

    work

---
+++ src/main/java/com/ll/exam20240205/Exam20240205Application.java
@@ -0,0 +1,13 @@
+package com.ll.exam20240205;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Exam20240205Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Exam20240205Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/ll/exam20240205/Exam20240205ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.exam20240205;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Exam20240205ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
